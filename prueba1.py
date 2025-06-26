8e53a4d3ae622c8bbc7786fbfdf3e06aac3116d7
agustin rocha <217123668+aguztin692@users.noreply.github.com>
2025-06-26 16:49:48 -0600

    remove prueba1.py

--- prueba1.py
+++
@@ -1,8 +0,0 @@
-# saludo.py
-
-def main():
-    nombre = input("¿Cuál es tu nombre, caballero? ")
-    print(f"Un placer saludarle, {nombre}. ¡que tenga un lindo dia!")
-
-if __name__ == "__main__":
-    main()
