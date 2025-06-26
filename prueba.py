1e74ff19673d64f3ff86a2c7b7e6e9a996f124e3
agustin rocha <217123668+aguztin692@users.noreply.github.com>
2025-06-26 16:46:16 -0600

    edit prueba.py

---
+++ prueba.py
@@ -0,0 +1,8 @@
+# saludo.py
+
+def main():
+    nombre = input("¿Cuál es tu nombre, caballero? ")
+    print(f"Un placer saludarle, {nombre}. ¡Que tenga un lindo dia!")
+
+if __name__ == "__main__":
+    main()
